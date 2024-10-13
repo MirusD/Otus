b76a3df9e5d87cb8fcceaad8d7d1af08995a1c1a
salihov_dr <damir.salihov@inbox.ru>
2024-10-13 15:51:58 +0500

    Create project

---
+++ DelegatesEventsAsync/DelegatesEventsAsync/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DelegatesEventsAsync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
