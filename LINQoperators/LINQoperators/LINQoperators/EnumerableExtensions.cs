7be95f3dff6bec62280dc05dd6e59720b335860b
salihov_dr <damir.salihov@inbox.ru>
2024-10-28 17:12:20 +0500

    Added EnumerableExtensions and Person classes

---
+++ LINQoperators/LINQoperators/LINQoperators/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace LINQoperators
+{
+    static class EnumerableExtensions
+    {
+        public static IEnumerable<TSource> Top<TSource>(this IEnumerable<TSource> source, double percent)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (percent <= 0 || percent > 100)
+                throw new ArgumentException($"Неверное значение аргумента percent = {percent}. " +
+                    $"Аргумент percent должен иметь значение от 0 до 100");
+
+            int n = (int)Math.Ceiling(source.Count() * (percent / 100));
+            var sortedList = source.OrderByDescending(element => element).ToList();
+
+            return sortedList.Take(n);
+        }
+
+        public static IEnumerable<TSource> Top<TSource, TKey>(this IEnumerable<TSource> source, double percent, Func<TSource, TKey> keySelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+            if (percent <= 0 || percent > 100)
+                throw new ArgumentException($"Неверное значение аргумента percent = {percent}. " +
+                    $"Аргумент percent должен иметь значение от 0 до 100");
+
+            int n = (int)Math.Ceiling(source.Count() * (percent / 100));
+            var sortedList = source.OrderByDescending(keySelector).ToList();
+
+            return sortedList.Take(n);
+        }
+    }
+}
---
+++ LINQoperators/LINQoperators/LINQoperators/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LINQoperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var sortedListByPercentage = list.Top(30);
+
+            string res = String.Join(",", sortedListByPercentage);
+
+            Console.WriteLine(res);
+
+            var persons = new List<Person>
+            {
+               new Person { Name = "Петрович", Age = 55 },
+               new Person { Name = "Иваныч", Age = 68 },
+               new Person { Name = "Михалыч", Age = 40 },
+               new Person { Name = "Алексеич", Age = 20 },
+               new Person { Name = "Махмутыч", Age = 18 },
+               new Person { Name = "Николаыч", Age = 28 },
+               new Person { Name = "Пупкиныч", Age = 38 },
+               new Person { Name = "Василич", Age = 32 },
+               new Person { Name = "Александрыч", Age = 45 }
+            };
+
+            var sortPersons = persons.Top(30, person => person.Age);
+
+            Console.WriteLine("-----------------------------------");
+            foreach (var person in sortPersons)
+            {
+                Console.WriteLine($"{person.Name} : {person.Age}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
